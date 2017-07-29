7221640232b14989a3198808b530a73e9f132f65
Nathan Peterson <nalexpeterson@gmail.com>
2017-07-29 18:00:42 -0400

    Basic firebase setup

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+    firebase: {
+      apiKey: "AIzaSyDz9pA7RaL093ABxm_gGfteduG0TQRLC6s",
+      authDomain: "dealingwithcancer-2ff25.firebaseapp.com",
+      databaseURL: "https://dealingwithcancer-2ff25.firebaseio.com",
+      projectId: "dealingwithcancer-2ff25",
+      storageBucket: "dealingwithcancer-2ff25.appspot.com",
+      messagingSenderId: "990697567358"
+    }
 };
